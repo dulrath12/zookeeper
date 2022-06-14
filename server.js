5dc520597a9cbcb401dc922bb11534b0a7de33e1
Dakotah Ulrath <dulrath@live.com>
2022-06-13 23:10:47 -0400

    code cleanup

--- server.js
+++ server.js
@@ -1,58 +1,58 @@
-const { query } = require('express')
-const express = require('express')
-const res = require('express/lib/response')
-const app = express()
-const {animals} = require('./data/animals.json')
-const PORT = process.env.PORT || 3001
+const express = require('express');
+const { animals } = require('./data/animals');
 
-app.listen(PORT, () => {
-    console.log(`API now available on port ${PORT}`)
-})
-
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 function filterByQuery(query, animalsArray) {
-    let filteredResults = animalsArray;
-    let personalityTraitsArray = []
-    if (query.personalityTraits) {
-        if (typeof query.personalityTraits === 'string') {
-            personalityTraitsArray = [query.personalityTraits]
-        } else {
-            personalityTraitsArray = query.personalityTraits
-        }
-        personalityTraitsArray.forEach(trait => {
-            filteredResults = filteredResults.filter(animal => animal.personalityTraits.indexOf(trait) !== -1)
-        })
+  let personalityTraitsArray = [];
+  let filteredResults = animalsArray;
+  if (query.personalityTraits) {
+    if (typeof query.personalityTraits === 'string') {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
     }
-    if (query.diet) {
-      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-    if (query.species) {
-      filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    if (query.name) {
-      filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    return filteredResults;
+    personalityTraitsArray.forEach(trait => {
+      filteredResults = filteredResults.filter(
+        animal => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
   }
+  if (query.diet) {
+    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+  }
+  return filteredResults;
+}
 
 function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0]
-    return result
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
 }
 
 app.get('/api/animals', (req, res) => {
-    let results = animals
-    if (req.query) {
-        results = filterByQuery(req.query, results)
-    }
-    res.json(results)
-})
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
 
 app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals)
-    if (result) {
-        res.json(result)
-    } else {
-        res.send(404)
-    }
-})
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
