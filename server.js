ea80748b0a425128c201c264f823615701a4e76b
Dakotah Ulrath <dulrath@live.com>
2022-06-13 22:44:05 -0400

    add heroku

---
+++ server.js
@@ -0,0 +1,42 @@
+const { query } = require('express')
+const express = require('express')
+const res = require('express/lib/response')
+const app = express()
+const {animals} = require('./data/animals.json')
+
+app.listen(3001, () => {
+    console.log('API server now on port 3001!')
+})
+
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    let personalityTraitsArray = []
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits]
+        } else {
+            personalityTraitsArray = query.personalityTraits
+        }
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(animal => animal.personalityTraits.indexOf(trait) !== -1)
+        })
+    }
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+  }
+
+app.get('/api/animals', (req, res) => {
+    let results = animals
+    if (req.query) {
+        results = filterByQuery(req.query, results)
+    }
+    res.json(results)
+})
