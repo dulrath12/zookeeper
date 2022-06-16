b453c1229244db3434076c2708251ab18dba5ae9
Dakotah Ulrath <dulrath@live.com>
2022-06-15 20:22:02 -0400

    lesson code snapshot

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
-const fs = require('fs')
-const path = require('path')
 const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// parse incoming string or array
-app.use(express.urlencoded({extended: true}))
-// parse incoming JSON data
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -44,32 +42,29 @@ function findById(id, animalsArray) {
 }
 
 function createNewAnimal(body, animalsArray) {
-  // our function's main code will go here
-  const animal = body
-  animalsArray.push(animal)
+  const animal = body;
+  animalsArray.push(animal);
   fs.writeFileSync(
     path.join(__dirname, './data/animals.json'),
-    JSON.stringify({animals: animalsArray}, null, 2)
-  )
-
-  // return finished code to post route for response
-  return animal
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
 }
 
 function validateAnimal(animal) {
   if (!animal.name || typeof animal.name !== 'string') {
-    return false
+    return false;
   }
   if (!animal.species || typeof animal.species !== 'string') {
-    return false
+    return false;
   }
   if (!animal.diet || typeof animal.diet !== 'string') {
-    return false
+    return false;
   }
-  if (!animal.personalityTraits || typeof animal.personalityTraits !== 'string') {
-    return false
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
   }
-  return true
+  return true;
 }
 
 app.get('/api/animals', (req, res) => {
@@ -91,19 +86,15 @@ app.get('/api/animals/:id', (req, res) => {
 
 app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
-  req.body.id = animals.length.toString()
+  req.body.id = animals.length.toString();
 
-  // if any data in the req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.')
+    res.status(400).send('The animal is not properly formatted.');
   } else {
-      // add animal to json file and animals array in this function
-  const animal = createNewAnimal(req.body, animals)
-
-  res.json(animal)
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
   }
-})
-
+});
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
